1381eec1964027fef58d113ee614b84a45a4d43c
Rahul <rahulsharmacs50@gmail.com>
2023-05-17 16:41:51 +0200

    streamlit check

--- src/pages/1_1._Bayesian_Networks.py
+++ src/pages/1_1._Bayesian_Networks.py
@@ -7,4 +7,5 @@ st.markdown(
 In this chapter you will learn about the following:\n
 1. The connection between graphs and probability theory.
 2. How we can use a graph to consistently define a family of probability distributions with a well defined set of properties. 
+3. checking streamlit
 ''')
